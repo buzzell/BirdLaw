1c51ea36ff0e3957989dd934e140ecdfcc9ee0d4
Justin <sciencenut101@gmail.com>
2020-07-04 02:51:40 -0700

    Update for heroku

--- index.php
+++ index.php
@@ -27,16 +27,16 @@
         <meta property="og:type" content="website" />
         <meta property="og:title" content="<?=$t['title']?>" />
         <meta property="og:description" content="<?=$t['quote']?>" />
-        <meta property="og:url" content="http://birdlaw.buzzell.me/<?=$t['slug']?>" />
-        <meta property="og:image" content="http://birdlaw.buzzell.me/<?=$t['slug']?>.jpg" />
-        <meta property="og:audio" content="http://birdlaw.buzzell.me/<?=$t['slug']?>.ogg" />
+        <meta property="og:url" content="https://birdlaw.herokuapp.com/<?=$t['slug']?>" />
+        <meta property="og:image" content="https://birdlaw.herokuapp.com/<?=$t['slug']?>.jpg" />
+        <meta property="og:audio" content="https://birdlaw.herokuapp.com/<?=$t['slug']?>.ogg" />
         <meta name="theme-color" content="#000000">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
         <link rel="icon" type="image/png" href="/favicon-32x32.png?" sizes="32x32" />
         <link rel="icon" type="image/png" href="/favicon-16x16.png?" sizes="16x16" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="alternate" hreflang="en" href="http://birdlaw.buzzell.me" />
-        <link rel="canonical" href="http://birdlaw.buzzell.me/<?=$t['slug']?>" />
+        <link rel="canonical" href="https://birdlaw.herokuapp.com/<?=$t['slug']?>" />
         <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Oswald:700,400' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/normalize.css">
@@ -61,7 +61,7 @@
         </div>
         <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-40856113-4', 'auto');ga('send', 'pageview');</script>
         <!--[if lt IE 9]><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script><![endif]-->
-        <!--[if gte IE 9]><!--><script src="http://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script><!--<![endif]-->
+        <!--[if gte IE 9]><!--><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script><!--<![endif]-->
         <script src="js/jm.js"></script>
         <script src="js/list.js"></script>
         <script src="js/script.js"></script>
